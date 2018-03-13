abaed111e6cbd2bf378f8fb4700a954eeb639b07
gmontantez <gmontantez7@gmail.com>
2018-03-13 16:38:08 -0400

    Coin Changer Nickels and Pennies

--- coin_changer_ex.rb
+++ coin_changer_ex.rb
@@ -1,3 +1,14 @@
 def coin_changer(cents)
-	my_hash = {}
+	coins = {}
+	if cents >= 5
+		coins["nickel"] = cents / 5
+		cents = cents % 5
+		p cents
+	end
+
+	if cents >= 1
+		coins["penny"] = cents
+        
+	end
+	coins
 end
--- coin_changer_ex_test.rb
+++ coin_changer_ex_test.rb
@@ -10,4 +10,21 @@ class Codeword_exercise < Minitest::Test
 		call_hash = coin_changer(11) 
 		assert_equal(Hash, call_hash.class)
 	end
+	def test_for_one_cent_one_penny
+		call_hash = coin_changer(1)
+		assert_equal({"penny" => 1 }, call_hash)
+	end
+	def test_for_two_cents_two_penny
+		call_hash = coin_changer(2)
+		assert_equal({"penny" => 2 }, call_hash)
+	end
+	def test_for_5_cents_nickel
+		call_hash = coin_changer(5)
+		assert_equal({"nickel" => 1 }, call_hash)
+	end
+	def test_for_7_cents_one_nickel_two_penny
+		call_hash = coin_changer(7)
+		assert_equal({"nickel" => 1, "penny" => 2}, call_hash)
+	end
+
 end
